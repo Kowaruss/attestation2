9428738a778f06478d693f5ef82c802138007d8e
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2026-01-18 14:28:38 +0300

    Create script.js

---
+++ description/script.js
@@ -0,0 +1,26 @@
+const usernameSpan = document.getElementById("username");
+const userIcon = document.getElementById("userIcon");
+const dropdown = document.getElementById("dropdown");
+const logoutBtn = document.getElementById("logout");
+const startBtn = document.getElementById("startBtn");
+
+if (localStorage.getItem("isAuth") !== "true") {
+  window.location.href = "../index.html";
+}
+
+const username = localStorage.getItem("username");
+usernameSpan.textContent = username;
+
+userIcon.addEventListener("click", () => {
+  dropdown.classList.toggle("hidden");
+});
+
+logoutBtn.addEventListener("click", () => {
+  localStorage.removeItem("isAuth");
+  localStorage.removeItem("username");
+  window.location.href = "../index.html";
+});
+
+startBtn.addEventListener("click", () => {
+  window.location.href = "../stage1-bet-counter/index.html";
+});
