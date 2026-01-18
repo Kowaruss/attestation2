ec5cf31c5a7e01fa6c633534139b0cbb2ff0e521
Kowaruss <76119984+Kowaruss@users.noreply.github.com>
2026-01-18 14:20:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function login() {
+  const login = document.getElementById("login").value;
+  const password = document.getElementById("password").value;
 
+  if (login === "admin" && password === "admin") {
+    window.location.href = "../next/";
+  } else {
+    alert("Неверный логин или пароль");
+  }
+}
+
+function register() {
+  alert("Регистрация пока не реализована");
+}
